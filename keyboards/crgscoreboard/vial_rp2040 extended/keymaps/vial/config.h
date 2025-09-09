a63c5944daedf7cea4bff086145be96dd457f2a9
DefenestratorNZ <45814664+DefenestratorNZ@users.noreply.github.com>
2025-09-10 09:57:49 +1200

    extended keyboard setup

---
+++ keyboards/crgscoreboard/vial_rp2040 extended/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE0, 0xFC, 0xA8, 0x17, 0x40, 0xDD, 0xC7, 0x8E}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+#define DYNAMIC_KEYMAP_MACRO_COUNT 40
